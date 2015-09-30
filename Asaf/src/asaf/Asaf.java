8be519a53a4eb634489f4eaa3a6bfe9b47a96ee2
Juan Jose Tejeda Hernandez <juantjeda@gmail.com>
2015-09-30 17:52:54 -0600

    Cambios
    Hola Planeta Tierra...

--- Asaf/src/asaf/Asaf.java
+++ Asaf/src/asaf/Asaf.java
@@ -17,7 +17,7 @@ public class Asaf {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.print("Prueba de MAnejo");
+        System.out.print("Segunda prueba de Manejo");
     }
     
 }
